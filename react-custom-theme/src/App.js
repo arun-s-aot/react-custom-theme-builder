a5213d595b18872eef8a44195ed26962d65a8b2c
Arun S <arun.s@aot-technologies.com>
2022-08-12 14:57:04 +0530

    Initial workspace commit

---
+++ react-custom-theme/src/App.js
@@ -0,0 +1,17 @@
+import LoginForm from './components/Login';
+import './App.css';
+
+import React from 'react';
+
+import ThemeTest from './components/ThemeTest';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ThemeTest/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
